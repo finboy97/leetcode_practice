bab21c4b0ce0ab09bba2adb847aac753d2a10af3
Finbar Evans <finbar@kfwxl64hp9.home>
2023-04-10 13:16:22 +0100

    initial commit

---
+++ medium/directory_3/longest_substring_without_repeating_chars.py
@@ -0,0 +1,27 @@
+
+# Given a string s, find the length of the longest substring without repeating characters.
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        used_chars = set()
+        current_length=1
+        longest_substring=1
+        current_pos = 0
+        len_s = len(s)
+        while current_pos<len_s:
+            element_to_end = s[current_pos+1:]
+            used_chars.add(s[current_pos])
+            for cur_char in element_to_end:
+                if cur_char in used_chars:
+                    current_length=1
+                    used_chars.clear()
+                    break
+                else:
+                    used_chars.add(cur_char)
+                    current_length+=1
+                    if current_length > longest_substring:
+                        longest_substring=current_length
+            current_pos+=1
+            if longest_substring > (len(s)-current_pos):
+                break
+
+        return longest_substring
---
+++ medium/directory_3/test_3_longest_substring_without_repeating_chars.py
@@ -0,0 +1,14 @@
+from unittest import TestCase
+from .longest_substring_without_repeating_chars import Solution
+
+class TestSolution(TestCase):
+    global test_solution
+    test_solution= Solution()
+
+    def test_length_of_all_same_character(self):
+        test_str="bbb"
+        self.assertEquals(Solution.lengthOfLongestSubstring(self,test_str),1)
+
+    def test_string_with_backtracking_for_longest(self):
+        test_str="dvdf"
+        self.assertEquals(Solution.lengthOfLongestSubstring(self,test_str),3)
